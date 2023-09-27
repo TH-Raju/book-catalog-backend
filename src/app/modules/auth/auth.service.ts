f012b86db503df27a789c4be02dad509b1db6548
TH-Raju <rajukhan894200@gmail.com>
2023-09-27 16:11:49 +0600

    [Password] hash the password

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,7 +1,14 @@
 import { User } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import config from '../../../config';
 import prisma from '../../../shared/prisma';
 
 const createAuthUser = async (data: User): Promise<User | null> => {
+  const password = await bcrypt.hash(
+    data?.password,
+    Number(config.bycrypt_salt_rounds)
+  );
+  data.password = password;
   const result = await prisma.user.create({
     data,
   });
